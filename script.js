7badaf98656ea8124b80efdb16e8cdae49f2c0c0
MariaScript <mariap.sousa@undf.edu.br>
2025-05-13 16:07:31 -0300

    segundo commit - Arquivos do projeto

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-  $('.btn-primary').click(function () {
-    alert("Produto adicionado ao carrinho!");
+// Mensagem ao clicar no botão "Comprar"
+document.addEventListener("DOMContentLoaded", function () {
+  const botoesComprar = document.querySelectorAll(".btn-primary");
+
+  botoesComprar.forEach((botao) => {
+    botao.addEventListener("click", function (event) {
+      event.preventDefault(); // Evita que a página recarregue
+      alert("Produto adicionado ao carrinho!");
+    });
   });
 });
